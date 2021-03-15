b180331610aafdab4a991a09d5572d75e13b2f5a
Kalli <karlpv213@gmail.com>
2021-03-15 12:13:35 +0000

    Initial commit

---
+++ æfingaverkefni/æfing1/Assets/scriptur/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * 20 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += -transform.forward * 20 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * 20 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += -transform.right * 20 * Time.deltaTime;
+        }
+    }
+}
