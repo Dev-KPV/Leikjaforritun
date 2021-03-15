2709226e0d73f962506d9b3dfa74d3d278cdcdad
Kalli <karlpv213@gmail.com>
2021-03-15 12:41:00 +0000

    updarte

--- æfingaverkefni/æfing1/Assets/scriptur/PlayerMovement.cs
+++ æfingaverkefni/æfing1/Assets/scriptur/PlayerMovement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody leikmadur;
+    public float hradi = 20;
+    public float hlidarhradi = 20;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ///vantar eitthvað hér
     }
 
     // Update is called once per frame
@@ -15,19 +18,20 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += transform.forward * 20 * Time.deltaTime;
+            transform.position += transform.forward * hradi * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += -transform.forward * 20 * Time.deltaTime;
+            transform.position += -transform.forward * hradi* Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += transform.right * 20 * Time.deltaTime;
+            transform.position += transform.right * hlidarhradi * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += -transform.right * 20 * Time.deltaTime;
+            transform.position += -transform.right * hlidarhradi * Time.deltaTime;
         }
+        
     }
 }
