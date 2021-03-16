79064f6034eda1e250574cdc2a1220e686293b49
Kalli <karlpv213@gmail.com>
2021-03-16 15:26:01 +0000

    æfingaverkefni1
    updated 3.16.2021

--- æfingaverkefni/æfing1/Assets/scriptur/PlayerMovement.cs
+++ æfingaverkefni/æfing1/Assets/scriptur/PlayerMovement.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody leikmadur;
     public float hradi = 20;
     public float hlidarhradi = 20;
+    private Rigidbody leikmadur;
+    private int count;
+    public Text countText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        ///vantar eitthvað hér
+        Debug.Log("Byrja");
+        leikmadur = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
@@ -34,4 +43,31 @@ public class PlayerMovement : MonoBehaviour
         }
         
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "hlutur") // ef player snertir tag = hlutur
+        {
+            collision.collider.gameObject.SetActive(false);
+            count = count + 1; // teljarinn
+            Debug.Log("Nú er ég komin með " + count); // skrifar í console stig
+            SetCountText(); // kallar á fallið
+
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Stig: " + count.ToString();
+        if (count >= 4)
+            countText.text = "Þú hefur unnið með" + count.ToString() + " stigum";
+            // endurræsa;
+    }       
+
+    public void Endurraesa()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(0);
+    }
 }
+
+
