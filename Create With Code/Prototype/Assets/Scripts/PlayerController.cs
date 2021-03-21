5d22a1177f414619df74f1271d333e97d7b4ff1a
Kalli <karlpv213@gmail.com>
2021-03-21 22:05:30 +0000

    i

---
+++ Create With Code/Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Private Færibreytur
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // hér kemur player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // hér kemur hreyfing
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+    }
+}
